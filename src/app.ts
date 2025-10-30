64c23b9ecfad8d63bb6fe85e81d49f50e0e678f5
Your Name <anondo554@gmail.com>
2025-10-30 19:44:26 +0600

    part-3 complite

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,21 @@ import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
 import config from './config';
 import router from './app/routes';
+import cookieParser from 'cookie-parser'
+
 
 const app: Application = express();
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3001',
     credentials: true
 }));
 
 //parser
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.use("/api/v1", router)
+app.use("/api/v1", router);
 
 app.get('/', (req: Request, res: Response) => {
     res.send({
@@ -26,7 +28,6 @@ app.get('/', (req: Request, res: Response) => {
         timeStamp: new Date().toISOString()
     })
 });
-
 
 app.use(globalErrorHandler);
 
