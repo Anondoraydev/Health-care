35b66e82bfbbe0520e27f705b1a043f2655f1cdf
Your Name <anondo554@gmail.com>
2025-11-29 22:06:16 +0600

    update code

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,12 @@
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
 import express, { Application, NextFunction, Request, Response } from 'express';
-import httpStatus from 'http-status';
+import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
-import { PaymentController } from './app/modules/payment/payment.controller';
-import router from './app/routes';
+import notFound from './app/middlewares/notFound';
+import config from './config';
+import { uptime } from 'process';
+import { timeStamp } from 'console';
 
 const app: Application = express();
-app.use(cookieParser());
-app.post(
-    "/webhook",
-    express.raw({ type: "application/json" }),
-    PaymentController.handleStripeWebhookEvent
-);
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
@@ -21,38 +15,20 @@ app.use(cors({
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(sanitizeInput); // ADD THIS LINE
 
-
-
-// cron.schedule('* * * * *', () => {
-//     try {
-//         AppointmentService.cancelUnpaidAppointments();
-//     }
-//     catch (err) {
-//         console.error(err);
-//     }
-// });
 
 app.get('/', (req: Request, res: Response) => {
     res.send({
-        Message: "Ph health care server.."
+        message: "Server is running..",
+        environment: config.node_env,
+        uptime: process.uptime().toFixed(2) + " sec",
+        timeStamp: new Date().toISOString()
     })
 });
 
-app.use('/api/v1', router);
 
 app.use(globalErrorHandler);
 
-app.use((req: Request, res: Response, next: NextFunction) => {
-    res.status(httpStatus.NOT_FOUND).json({
-        success: false,
-        message: "API NOT FOUND!",
-        error: {
-            path: req.originalUrl,
-            message: "Your requested path is not found!"
-        }
-    })
-})
+app.use(notFound);
 
 export default app;
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,77 +1,12 @@
 import express from 'express';
-import { userRoutes } from '../modules/user/user.routes';
-import { AdminRoutes } from '../modules/admin/admin.routes';
-import { AuthRoutes } from '../modules/auth/auth.routes';
-import { SpecialtiesRoutes } from '../modules/specialties/specialties.routes';
-import { DoctorRoutes } from '../modules/doctor/doctor.routes';
-import { PatientRoutes } from '../modules/patient/patient.route';
-import { ScheduleRoutes } from '../modules/schedule/schedule.routes';
-import { DoctorScheduleRoutes } from '../modules/doctorSchedule/doctorSchedule.routes';
-import { AppointmentRoutes } from '../modules/appointment/appointment.routes';
-import { PaymentRoutes } from '../modules/payment/payment.routes';
-import { PrescriptionRoutes } from '../modules/prescription/prescription.routes';
-import { ReviewRoutes } from '../modules/review/review.routes';
-import { MetaRoutes } from '../modules/meta/meta.routes';
-import { apiLimiter } from '../middlewares/rateLimiter';
+
 
 const router = express.Router();
 
-
-
-router.use(apiLimiter); // Apply to all routes
-
 const moduleRoutes = [
     {
-        path: '/user',
-        route: userRoutes
-    },
-    {
-        path: '/admin',
-        route: AdminRoutes
-    },
-    {
-        path: '/auth',
-        route: AuthRoutes
-    },
-    {
-        path: '/specialties',
-        route: SpecialtiesRoutes
-    },
-    {
-        path: '/doctor',
-        route: DoctorRoutes
-    },
-    {
-        path: '/patient',
-        route: PatientRoutes
-    },
-    {
-        path: '/schedule',
-        route: ScheduleRoutes
-    },
-    {
-        path: '/doctor-schedule',
-        route: DoctorScheduleRoutes
-    },
-    {
-        path: '/appointment',
-        route: AppointmentRoutes
-    },
-    {
-        path: '/payment',
-        route: PaymentRoutes
-    },
-    {
-        path: '/prescription',
-        route: PrescriptionRoutes
-    },
-    {
-        path: '/review',
-        route: ReviewRoutes
-    },
-    {
-        path: '/meta',
-        route: MetaRoutes
+        path: '/',
+        route: router
     }
 ];
 
